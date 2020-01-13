b62f56dae6a246d4a2016a884bd320274e64beed
kuma_loveliver <kuma_loveliver@163.com>
2020-01-13 18:14:51 +0800

    使用新的分页api

--- src/main/java/org/starrysea/mail/common/util/PageQueryUtils.java
+++ src/main/java/org/starrysea/mail/common/util/PageQueryUtils.java
@@ -1,6 +1,8 @@
 package org.starrysea.mail.common.util;
 
 import cn.hutool.core.util.StrUtil;
+import com.github.pagehelper.ISelect;
+import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.starrysea.mail.common.entity.dto.PageQueryInfo;
@@ -11,16 +13,16 @@ import java.util.function.Supplier;
 
 public class PageQueryUtils {
 
-    public static <T> ListDataWithPageInfo<T> pageQuery(PageQueryInfo pageQueryInfo, Supplier<List<T>> supplier) {
+    public static <T> ListDataWithPageInfo<T> pageQuery(PageQueryInfo pageQueryInfo, ISelect select) {
+        Page<T> page;
         //如果前端传了orderBy字段,说明这次请求需要自定义排序
         if (StrUtil.isNotBlank(pageQueryInfo.getSortField())) {
-            PageHelper.startPage(pageQueryInfo.getPageNum(), pageQueryInfo.getPageSize(), pageQueryInfo.getSortField() + " " + pageQueryInfo.getSortType());
+            page = PageHelper.startPage(pageQueryInfo.getPageNum(), pageQueryInfo.getPageSize(), pageQueryInfo.getSortField() + " " + pageQueryInfo.getSortType());
         } else {
             //如果没有传,就直接使用普通的分页(默认排序就写在sql中,不需要pagehelper干涉)
-            PageHelper.startPage(pageQueryInfo.getPageNum(), pageQueryInfo.getPageSize());
+            page = PageHelper.startPage(pageQueryInfo.getPageNum(), pageQueryInfo.getPageSize());
         }
-        PageInfo<T> pageInfo = new PageInfo<>(supplier.get());
-        PageHelper.clearPage();
+        PageInfo<T> pageInfo = page.doSelectPageInfo(select);
         ListDataWithPageInfo<T> listDataWithPageInfo = new ListDataWithPageInfo<>();
         listDataWithPageInfo.setTotal(pageInfo.getTotal());
         listDataWithPageInfo.setRows(pageInfo.getList());
